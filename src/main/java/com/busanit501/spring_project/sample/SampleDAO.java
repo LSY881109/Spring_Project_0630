30e4b08cad2817d0fa6ad8c4898b3e6391262b08
이상용 <lsy3709@naver.com>
2025-06-30 14:34:52 +0900

    5교시, 느슨한 결합 방식 활용한 , 인터페이스 기본 구조 예시1

--- src/main/java/com/busanit501/spring_project/sample/SampleDAO.java
+++ src/main/java/com/busanit501/spring_project/sample/SampleDAO.java
@@ -2,6 +2,7 @@ package com.busanit501.spring_project.sample;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class SampleDAO {
+//@Repository
+// 클래스 -> 인터페이스로 변경. : 느슨한 설계 구조를 이용하기위해서
+public interface SampleDAO {
 }
---
+++ src/main/java/com/busanit501/spring_project/sample/SampleDAOImpl.java
@@ -0,0 +1,9 @@
+package com.busanit501.spring_project.sample;
+
+import org.springframework.stereotype.Repository;
+
+// 서버에, DAO 역할을 하는 자주 사용하는 객체야
+// 그래서,@Repository 등록해서 사용할게,
+@Repository
+public class SampleDAOImpl implements SampleDAO{
+}
