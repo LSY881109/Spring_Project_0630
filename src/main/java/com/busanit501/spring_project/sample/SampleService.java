83bdca704ba63053d3451c9541b38ded0fcfd868
이상용 <lsy3709@naver.com>
2025-06-30 11:48:42 +0900

    3교시, 새 스프링 프로젝트 , component-scan 등록 기초

--- src/main/java/com/busanit501/spring_project/sample/SampleService.java
+++ src/main/java/com/busanit501/spring_project/sample/SampleService.java
@@ -1,4 +1,14 @@
 package com.busanit501.spring_project.sample;
 
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ToString
 public class SampleService {
+
+    // 필드 주입,
+    // 루트 컨텍스트 의 파일 위치를 인식를 못해서, 컴파일러 오류상황
+
+//    @Autowired
+    private SampleDAO sampleDAO;
 }
