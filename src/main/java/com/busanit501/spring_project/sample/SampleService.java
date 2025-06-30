365099f3a68f5f71233165901f82045347db0606
이상용 <lsy3709@naver.com>
2025-06-30 15:20:56 +0900

    6교시, 느슨한 결합 방식 활용한 , 인터페이스 기본 구조 예시3, 해결책 2가지, 생성자 주입 방식과, @Qualifier 중복 문제 발생, 따로 생성자 구현해서 사용하기

--- src/main/java/com/busanit501/spring_project/sample/SampleService.java
+++ src/main/java/com/busanit501/spring_project/sample/SampleService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @ToString
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class SampleService {
 
     // 필드 주입,
@@ -21,8 +21,15 @@ public class SampleService {
     // 생성자 주입 방식. -> 롬복 이용하기. ->@RequiredArgsConstructor
     // 해결책2,
     // 우리가 지정한 라벨을 표기. 결론, 선택.
-    @Qualifier("normal")
+
+    // 중복 사용이 안되서 , 따로 분리해서 설정.
+//    @Qualifier("normal")
     private final  SampleDAO sampleDAO;
+
+//    해결책, 따로 재정의해서, 적용하기.
+    public SampleService( @Qualifier("normal") SampleDAO sampleDAO) {
+        this.sampleDAO = sampleDAO;
+    }
     // 문제점, 제시.
     // 1, SampleDAO 를 구현한 , 구현체 클래스가 1개였다가
     // 구현체 클래스가 2개인 상황.
