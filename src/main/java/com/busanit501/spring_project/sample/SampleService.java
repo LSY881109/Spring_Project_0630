ab4392eb44979d0ca6f512e8ad4bfefd739a12c8
이상용 <lsy3709@naver.com>
2025-06-30 14:21:06 +0900

    5교시, 생성자 주입방식의 기본 개념, 활용한 불변 객체를 주입 하는 예시

--- src/main/java/com/busanit501/spring_project/sample/SampleService.java
+++ src/main/java/com/busanit501/spring_project/sample/SampleService.java
@@ -1,16 +1,22 @@
 package com.busanit501.spring_project.sample;
 
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @ToString
+@RequiredArgsConstructor
 public class SampleService {
 
     // 필드 주입,
     // 루트 컨텍스트 의 파일 위치를 인식를 못해서, 컴파일러 오류상황
 
-    @Autowired
-    private SampleDAO sampleDAO;
+//    @Autowired
+//    private SampleDAO sampleDAO;
+
+    // 방법2,
+    // 생성자 주입 방식. -> 롬복 이용하기. ->@RequiredArgsConstructor
+    private final  SampleDAO sampleDAO;
 }
