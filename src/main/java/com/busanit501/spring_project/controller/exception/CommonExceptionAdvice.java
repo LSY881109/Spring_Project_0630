62a602c1e3ed9bd5aa595bd23b103a0ebedb704d
이상용 <lsy3709@naver.com>
2025-07-01 15:25:58 +0900

    6교시, 예외처리 예시1, 전역 예외처리 부서에서, 경우1 숫자 포맷 처리 방법1

--- src/main/java/com/busanit501/spring_project/controller/exception/CommonExceptionAdvice.java
+++ src/main/java/com/busanit501/spring_project/controller/exception/CommonExceptionAdvice.java
@@ -3,8 +3,19 @@ package com.busanit501.spring_project.controller.exception;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 @Log4j2
 public class CommonExceptionAdvice {
+
+    // 예외 처리 경우1, 숫자 포맷 예외처리.
+    @ResponseBody
+    @ExceptionHandler(NumberFormatException.class)
+    public String exceptNumber(NumberFormatException numberFormatException) {
+        log.error("===CommonExceptionAdvice에서, exceptNumber 담당 , 예외1=================");
+        log.error(numberFormatException.getMessage());
+        return "숫자 포맷 형식의 예외 발생입니다.";
+    }
 }
