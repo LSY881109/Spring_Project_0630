3031d8a613f7928540cbd9b6cf3439f743e93141
이상용 <lsy3709@naver.com>
2025-07-01 14:54:46 +0900

    6교시,개념 설명 및, 파라미터 자동 수집 관련, 서버에서 웹브라우저에게 데이터 전달 예시2, 자동 전달2

--- src/main/java/com/busanit501/spring_project/controller/SampleController.java
+++ src/main/java/com/busanit501/spring_project/controller/SampleController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
@@ -28,16 +29,16 @@ public class SampleController {
     // 화면에서 , 쿼리스트링 형식으로 , get 방식으로
     // 데이터 전달
     // http://localhost:8080/ex1?name=lsy&age=20
-    public void ex1(String name, int age){
+    public void ex1(String name, int age) {
         log.info("SampleController 작업중. ex1");
-        log.info("name:"+name+",age:"+age);
+        log.info("name:" + name + ",age:" + age);
     }
 
     @GetMapping("/ex2")
     public void ex2(@RequestParam(name = "name", defaultValue = "사용자") String name,
                     @RequestParam(name = "age", defaultValue = "20") int age) {
         log.info("SampleController 작업중. ex2, 값이 없는 경우, 기본값 이용");
-        log.info("name:"+name+",age:"+age);
+        log.info("name:" + name + ",age:" + age);
     }
 
     // 날짜 포맷터 인경우.
@@ -48,7 +49,7 @@ public class SampleController {
     @GetMapping("/ex3")
     public void ex3(LocalDate dueDate) {
         log.info("SampleController 작업중. ex3, 날짜 포맷터 형식인 경우");
-        log.info("dueDate:"+dueDate);
+        log.info("dueDate:" + dueDate);
     }
 
     // 서버 -> 웹 화면, 데이터 전달.
@@ -66,7 +67,11 @@ public class SampleController {
     @GetMapping("/ex4_1")
     // TodoDTO , 모델 클래스는 , 자동으로 화면까지 객체를 전달.
     // 자동 전달.
-    public void ex4_1(TodoDTO todoDTO, Model model) {
+    //기본 전달
+//    public void ex4_1(TodoDTO todoDTO, Model model) {
+    // todoDTO 이름 말고, dto 라는 이름으로 전달하기.
+    // 화면에서는 사용시, todoDTO 아니라, dto라는 이름으로 이용하기.
+    public void ex4_1(@ModelAttribute("dto") TodoDTO todoDTO, Model model) {
         todoDTO.setTitle("임시제목만 입력");
         log.info("SampleController 작업중. ex4_1,서버 -> 웹 화면 데이터 전달");
         log.info("데이터 탑재를 하는 부분이 없는데, 화면에서, 해당 todoDTO를 자동으로 이용가능함");
