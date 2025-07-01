4e5b9d812c12b3432cfed94e8802c246b698100a
이상용 <lsy3709@naver.com>
2025-07-01 11:05:38 +0900

    3교시, 컨트롤러 이용해서, /todo/register 화면 연결 ,

--- src/main/java/com/busanit501/spring_project/controller/TodoController.java
+++ src/main/java/com/busanit501/spring_project/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.busanit501.spring_project.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @Log4j2
@@ -15,10 +16,19 @@ public class TodoController {
     // 최종 url : /todo/list
     @RequestMapping("/list")
     // void 라고하면, 메서드 명 : list
-    // /WEB-INF/views/list.jsp , 가리킴.
+    // /WEB-INF/views/todo/list.jsp , 가리킴.
     // 자동 연결, 뷰 리졸버라는 친구의 업무.
     public void list(){
         log.info("TodoController에서 작업, list 호출 ");
+    }
+
+    // 최종 url : /todo/register
+    // void 라고하면, 메서드 명 : register
+    // /WEB-INF/views/todo/register.jsp , 가리킴.
+    // 자동 연결, 뷰 리졸버라는 친구의 업무.
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public void register(){
+        log.info("TodoController에서 작업, register 호출 ");
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/todo/register.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: it
+  Date: 25. 7. 1.
+  Time: 오전 11:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+<h1>Register 임시 화면</h1>
+</body>
+</html>
