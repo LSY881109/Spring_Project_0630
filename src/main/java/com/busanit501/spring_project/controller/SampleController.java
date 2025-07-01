b146caa606d5df1afc33b26137df5537ee29074b
이상용 <lsy3709@naver.com>
2025-07-01 10:32:28 +0900

    2교시, mvc에서, 서블릿 컨텍스트 설정에서, 지정한 뷰 폴더 위치와 컨트롤러 연결 하기, 예시1, 설명 추가

--- src/main/java/com/busanit501/spring_project/controller/SampleController.java
+++ src/main/java/com/busanit501/spring_project/controller/SampleController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SampleController {
     // 1)화면 제공, 2) 로직처리
     @GetMapping("/hello")
+//    반환형이 void인 경우, 요청 URL을 기준으로 
+//    JSP 페이지 이름 자동 매칭
+    // /WEB-INF/views/hello.jsp
+    // public void hello() , 동일 이름으로 화면 호출
     public void hello() {
         log.info("hello.......");
     }
