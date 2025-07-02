22090f05c809f5cfdda71a60922857cd6ca06c3d
이상용 <lsy3709@naver.com>
2025-07-02 16:40:08 +0900

    7교시, Todo 하나조회, 컨트롤러, 화면 연결 작업

--- src/main/java/com/busanit501/spring_project/controller/TodoController.java
+++ src/main/java/com/busanit501/spring_project/controller/TodoController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -77,6 +78,12 @@ public class TodoController {
         todoService.register(todoDTO);
 
         return "redirect:/todo/list";
+    }
+    @GetMapping("/read")
+    public void read(Long tno, Model model) {
+        TodoDTO todoDTO = todoService.selectByTno(tno);
+        log.info(todoDTO);
+        model.addAttribute("dto", todoDTO);
     }
 
 }
